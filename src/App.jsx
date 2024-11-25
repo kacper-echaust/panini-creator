4c31c0d743cd8f4218b90c625152a99b8f2d3b43
kacper-echaust <k9echaust@gmail.com>
2024-11-25 07:55:58 +0100

    add panini creator screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import { SplashScreen } from './Components/SplashScreen'
+import { SplashScreen } from './Components/SplashScreen/SplashScreen'
+import { Form } from './Components/Form/Form'
 
 function App() {
 	const [animate, setAnimate] = useState(false)
 
-	return <SplashScreen animate={animate} setAnimate={setAnimate}/>
+	return (
+		<>
+			<SplashScreen animate={animate} setAnimate={setAnimate} />
+			{animate && <Form animate={animate} />}
+		</>
+	)
 }
 
 export default App
---
+++ src/Components/Form/Form.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Form.css'
+type Props = {
+	animate: boolean
+}
+
+const Form = ({ animate }: Props) => {
+	console.log(animate)
+	return (
+		<div className={`container ${animate ? 'fadeIn' : ''}`}>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias ullam est, nobis maiores corporis
+			eligendi error veniam totam a sapiente porro, suscipit esse, perferendis laboriosam doloribus voluptatem aut
+			aperiam eum!
+		</div>
+	)
+}
+
+export { Form }
