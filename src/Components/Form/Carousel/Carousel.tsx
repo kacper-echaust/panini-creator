e2c180c8638249864f5deecb374923d531ab205d
kacper-echaust <k9echaust@gmail.com>
2024-11-26 16:51:04 +0100

    create reusable carousel component

---
+++ src/Components/Form/Carousel/Carousel.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { LeftArrow } from '../../../assets/icons/leftArrow'
+import { RightArrow } from '../../../assets/icons/rightArrow'
+
+type Props = {
+	data: string[]
+}
+
+const Carousel = ({ data }: Props) => {
+	const [value, setValue] = useState(0)
+	const nextValue = () => {
+		setValue(prevValue => {
+			if (prevValue >= data.length - 1) return 0
+			return prevValue + 1
+		})
+	}
+	const prevValue = () => {
+		setValue(prevValue => {
+			if (prevValue === 0) return data.length - 1
+			return prevValue - 1
+		})
+	}
+	return (
+		<div>
+			<LeftArrow onClick={prevValue} />
+			<input type='text' value={data[value]} disabled />
+			<RightArrow onClick={nextValue} />
+		</div>
+	)
+}
+
+export { Carousel }
---
+++ src/assets/icons/RightArrow.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+type Props = {
+	onClick: () => void
+}
+
+const RightArrow = ({ onClick }: Props) => {
+	return (
+		<svg
+			onClick={onClick}
+			style={{ cursor: 'pointer' }}
+			width='12'
+			height='22'
+			viewBox='0 0 12 22'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'>
+			<path id='Vector 4' d='M1 1L11 11L1 21' stroke='black' stroke-width='0.5' />
+		</svg>
+	)
+}
+
+export { RightArrow }
