fcd36aff5e6d3a0b8947bb8c2bcc1c8f3ffb5195
kacper-echaust <k9echaust@gmail.com>
2024-12-05 18:45:39 +0100

    create second form

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -6,9 +6,14 @@ import { cheeseVariants } from '../../data/cheese'
 import { meatVariants } from '../../data/meat'
 import { dressingVariants } from '../../data/dressing'
 import { vegetableVariant } from '../../data/vegetable'
+import { eggVariants } from '../../data/egg'
+import { spreadVariant } from '../../data/spread'
+import { servingVariant } from '../../data/serving'
+import { toppingVariant } from '../../data/topping'
 import { Switch } from './Switch/Switch'
 import { Select } from './Select/Select'
 import { Section } from './Section/Section'
+import { Checkbox } from './Checkbox/Checkbox'
 type Props = {
 	animate: boolean
 }
@@ -51,6 +56,44 @@ const Form = ({ animate }: Props) => {
 			</div>
 			<div className={css.container}>
 				<h3>configure extras</h3>
+				<Section name='Egg'>
+					<Switch id='egg'>
+						<Select name='egg' data={eggVariants} />
+					</Switch>
+				</Section>
+				<Section name='Spreads'>
+					<div>
+						{spreadVariant.map(spread => {
+							return (
+								<div className={css.spreadContainer}>
+									<Checkbox name={spread} />
+								</div>
+							)
+						})}
+					</div>
+				</Section>
+				<Section name='Serving'>
+					<div className={css.servingContainer}>
+						{servingVariant.map(serving => {
+							return (
+								<div className={css.servingContainer}>
+									<Checkbox name={serving} className={css.radiusCheckbox} />
+								</div>
+							)
+						})}
+					</div>
+				</Section>
+				<Section name='Topping'>
+					<div>
+						{toppingVariant.map(topping => {
+							return (
+								<div className={css.toppingContainer}>
+									<Checkbox name={topping} />
+								</div>
+							)
+						})}
+					</div>
+				</Section>
 			</div>
 			<div className={css.container}>
 				<h3>finalize order</h3>
