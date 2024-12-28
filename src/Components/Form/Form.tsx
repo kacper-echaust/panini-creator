7639e185abff0238155774cb2522d5c726087f68
kacper-echaust <k9echaust@gmail.com>
2024-12-28 11:33:33 +0100

    implement rhf into form UI

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -17,119 +17,139 @@ import { Checkbox } from './Checkbox/Checkbox';
 import dices from '../../assets/icons/Dices.jpg';
 import { GridSelector } from './GridSelector/GridSelector';
 import { PaniniContext, SplashScreenValues } from '../../context/PaniniContext';
+import { FormProvider, useForm } from 'react-hook-form';
 
 const Form = () => {
   const { animate, setAnimate, setSplashScreenValues } = useContext(PaniniContext);
+  const methods = useForm({
+    defaultValues: {
+      bread: breadVariants[0],
+      cheese: [{ value: cheeseVariants[0] }],
+      dressing: [{ value: dressingVariants[0] }],
+      meat: [{ value: meatVariants[0] }],
+      vegetables: [{ value: vegetableVariant[0] }],
+      egg: [{ value: eggVariants[0] }],
+      spreads: [{ value: spreadVariant[0] }],
+      serving: [{ value: servingVariant[0] }],
+      topping: [{ value: toppingVariant[0] }],
+      cutlery: [],
+      napkins: [],
+      paniniName: ''
+    },
+  });
 
-  const sendOrder = () => {
+  const sendOrder = (data) => {
     setAnimate(false);
     setSplashScreenValues({
       headerName: SplashScreenValues.PaniniOrdered,
       buttonName: SplashScreenValues.StartAgain,
     });
+    console.log(data);
   };
 
   return (
-    <form className={`${css.form} ${animate ? css.fadeIn : ''}`}>
-      <div className={css.header}>
-        <h1>Panini Creator</h1>
-      </div>
-      <div className={css.randomizePaniniButton}>
-        <button>
-          <img src={dices} alt="dices" />
-          randomize panini
-        </button>
-      </div>
-      <div className={css.container}>
-        <h3>configure base</h3>
-        <Section name="Bread">
-          <Carousel data={breadVariants} />
-        </Section>
-        <Section name="Cheese">
-          <Switch id="cheese">
-            <Select name="Cheese" data={cheeseVariants} />
-          </Switch>
-        </Section>
-        <Section name="Meat">
-          <Switch id="meat">
-            <Select name="Meat" data={meatVariants} />
-          </Switch>
-        </Section>
-        <Section name="Dressing">
-          <Switch id="dressing">
-            <Carousel data={dressingVariants} />
-          </Switch>
-        </Section>
-        <Section name="Vegetables">
-          <GridSelector data={vegetableVariant} />
-        </Section>
-      </div>
-      <div className={css.container}>
-        <h3>configure extras</h3>
-        <Section name="Egg">
-          <Switch id="egg">
-            <Select name="egg" data={eggVariants} />
-          </Switch>
-        </Section>
-        <Section name="Spreads">
-          <div>
-            {spreadVariant.map((spread, index) => {
-              return (
-                <div className={css.spreadContainer} key={index}>
-                  <Checkbox name={spread} />
-                </div>
-              );
-            })}
-          </div>
-        </Section>
-        <Section name="Serving">
-          <div className={css.servingContainer}>
-            {servingVariant.map((serving, index) => {
-              return (
-                <div className={css.servingContainer} key={index}>
-                  <Checkbox name={serving} className={css.radiusCheckbox} />
-                </div>
-              );
-            })}
-          </div>
-        </Section>
-        <Section name="Topping">
-          <div>
-            {toppingVariant.map((topping, index) => {
-              return (
-                <div className={css.toppingContainer} key={index}>
-                  <Checkbox name={topping} />
-                </div>
-              );
-            })}
-          </div>
-        </Section>
-      </div>
-      <div className={css.container}>
-        <h3>finalize order</h3>
-        <Section name="Name panini">
-          <label htmlFor="paniniName" className={css.inputPaniniName}>
-            <input id="paniniName" type="text" placeholder="eg. Club Panini" />
-          </label>
-        </Section>
-        <Section name="Cutlery">
-          <div className={css.addToOrderContainer}>
-            <Checkbox name="ADD TO ORDER" />
-          </div>
-        </Section>
-        <Section name="Napkins">
-          <div className={css.addToOrderContainer}>
-            <Checkbox name="ADD TO ORDER" />
-          </div>
-        </Section>
-        <div className={css.endButtonsContainer}>
-          <button className={css.placeOrderButton} onClick={sendOrder}>
-            place order
-          </button>
-          <button className={css.startAgainButton}>start again</button>
+    <FormProvider {...methods}>
+      <form className={`${css.form} ${animate ? css.fadeIn : ''}`} onSubmit={methods.handleSubmit(sendOrder)}>
+        <div className={css.header}>
+          <h1>Panini Creator</h1>
         </div>
-      </div>
-    </form>
+        <div className={css.randomizePaniniButton}>
+          <button>
+            <img src={dices} alt="dices" />
+            randomize panini
+          </button>
+        </div>
+        <div className={css.container}>
+          <h3>configure base</h3>
+          <Section name="Bread">
+            <Carousel data={breadVariants} name="bread" />
+          </Section>
+          <Section name="Cheese">
+            <Switch switchName="cheese">
+              <Select data={cheeseVariants} name="cheese" />
+            </Switch>
+          </Section>
+          <Section name="Meat">
+            <Switch switchName="meat">
+              <Select data={meatVariants} name="meat" />
+            </Switch>
+          </Section>
+          <Section name="Dressing">
+            <Switch switchName="dressing">
+              <Carousel data={dressingVariants} name="dressing" />
+            </Switch>
+          </Section>
+          <Section name="Vegetables">
+            <GridSelector data={vegetableVariant} name="vegetables" />
+          </Section>
+        </div>
+        <div className={css.container}>
+          <h3>configure extras</h3>
+          <Section name="Egg">
+            <Switch switchName="egg">
+              <Select name="egg" data={eggVariants} />
+            </Switch>
+          </Section>
+          <Section name="Spreads">
+            <div>
+              {spreadVariant.map((spread, index) => {
+                return (
+                  <div className={css.spreadContainer} key={index}>
+                    <Checkbox valueName={spread} name="spreads" />
+                  </div>
+                );
+              })}
+            </div>
+          </Section>
+          <Section name="Serving">
+            <div className={css.servingContainer}>
+              {servingVariant.map((serving, index) => {
+                return (
+                  <div className={css.servingContainer} key={index}>
+                    <Checkbox name="serving" className={css.radiusCheckbox} valueName={serving} />
+                  </div>
+                );
+              })}
+            </div>
+          </Section>
+          <Section name="Topping">
+            <div>
+              {toppingVariant.map((topping, index) => {
+                return (
+                  <div className={css.toppingContainer} key={index}>
+                    <Checkbox name="topping" valueName={topping} />
+                  </div>
+                );
+              })}
+            </div>
+          </Section>
+        </div>
+        <div className={css.container}>
+          <h3>finalize order</h3>
+          <Section name="Name panini">
+            <label htmlFor="paniniName" className={css.inputPaniniName}>
+              <input id="paniniName" type="text" placeholder="eg. Club Panini" {...methods.register('paniniName')}/>
+            </label>
+          </Section>
+          <Section name="Cutlery">
+            <div className={css.addToOrderContainer}>
+              <Checkbox valueName="ADD TO ORDER" name="cutlery" />
+            </div>
+          </Section>
+          <Section name="Napkins">
+            <div className={css.addToOrderContainer}>
+              <Checkbox valueName="ADD TO ORDER" name="napkins" />
+            </div>
+          </Section>
+          <div className={css.endButtonsContainer}>
+            <button type="submit" className={css.placeOrderButton}>
+              place order
+            </button>
+            <button className={css.startAgainButton}>start again</button>
+          </div>
+        </div>
+      </form>
+    </FormProvider>
   );
 };
 
--- src/Components/Form/GridSelector/GridSelector.tsx
+++ src/Components/Form/GridSelector/GridSelector.tsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import css from './GridSelector.module.css';
+import { useFieldArray, useFormContext } from 'react-hook-form';
 
 type Props = {
   data: string[];
+  name: string;
 };
 
-const GridSelector = ({ data }: Props) => {
+const GridSelector = ({ data, name }: Props) => {
+  const { control, watch } = useFormContext();
+  const { append, remove } = useFieldArray({
+    control,
+    name: name,
+  });
+  const selectedValues = watch(name) || [];
+
+  const handleSelect = (item: string) => {
+    const isSelected = selectedValues.some((field: { value: string }) => field.value === item);
+
+    if (isSelected) {
+      const indexToRemove = selectedValues.findIndex((field: { value: string }) => field.value === item);
+      remove(indexToRemove);
+    } else {
+      append({ value: item });
+    }
+  };
   return (
     <div className={css.container}>
       {data.map((item, index) => {
         return (
           <div key={index}>
-            <input type="checkbox" id={item} />
-            <label htmlFor={item}> {item}</label>
+            <input
+              type="checkbox"
+              id={`${name}-${index}`}
+              checked={selectedValues.some((field: { value: string }) => field.value === item)}
+              onChange={() => {
+                handleSelect(item);
+              }}
+            />
+            <label htmlFor={`${name}-${index}`}> {item}</label>
           </div>
         );
       })}
